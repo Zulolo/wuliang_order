f0460ffd7f41a657278d71e6a74c28ad27241e41
Zulolo <dodoleon@sina.com>
2017-09-12 21:59:13 +0800

    remove track of modules

--- server/config/db.js
+++
@@ -1,7 +0,0 @@
-var mysql      = require('mysql');
-var mysql_conn = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'Aa1234',
-  database : 'wuliang_order'
-});
