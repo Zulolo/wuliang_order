67fe2650cda49fb52ba157bbc3db0290f599d2b0
zulolo <dodoleon@sina.com>
2017-09-12 19:16:19 +0800

    start build the structure

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+var mysql      = require('mysql');
+var mysql_conn = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Aa1234',
+  database : 'wuliang_order'
+});
