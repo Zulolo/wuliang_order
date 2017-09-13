c11c7f285bbfe9a01ff420a59f4d4f1936ec47de
Zulolo <dodoleon@sina.com>
2017-09-13 23:04:27 +0800

    adding routes in routes folder

--- server/routes/dishes.js
+++ server/routes/dishes.js
@@ -1,20 +1,36 @@
 'use strict';
 
-const Boom = require('boom');  
-const uuid = require('uuid');  
+const Boom = require('boom');
+const uuid = require('uuid');
 const Joi = require('joi');
 
 exports.register = function(server, options, next) {
 
-  const db = server.app.db;
+	const db = server.app.db;
 
-  //PLACEHOLDER
-  //--------------------------------------------------------------
-  //Here the routes definitions will be inserted in the next steps...
+	//PLACEHOLDER
+	//--------------------------------------------------------------
+	//Here the routes definitions will be inserted in the next steps...
+	server.route({
+		method: 'GET',
+		path: '/dishes',
+		handler: function(request, reply) {
 
-  return next();
+			db.books.find((err, docs) => {
+
+				if (err) {
+					return reply(Boom.wrap(err, 'Internal MongoDB error'));
+				}
+
+				reply(docs);
+			});
+
+		}
+	});
+
+	return next();
 };
 
-exports.register.attributes = {  
-  name: 'routes-dishes'
+exports.register.attributes = {
+	name: 'routes-dishes'
 };
--- server/server.js
+++ server/server.js
@@ -10,44 +10,58 @@ const server = new Hapi.Server();
 
 server.app.db =  mysql.createConnection(dbConfig);
 
-server.route({
-    method: 'GET',
-    path: '/wuliang_order',
-    handler: function (request, reply) {
-        reply('wu liang order home!');
-    }
-});
-
-server.route({
-    method: 'GET',
-    path: '/wuliang_order/menu/list_all',
-    handler: function (request, reply) {
-        reply(results);
-    }
-});
-
-server.route({
-    method: 'GET',
-    path: '/wuliang_order/{module}',
-    handler: function (request, reply) {
-        // reply('Module -- ' + encodeURIComponent(request.params.module) + ' under developing!');
-        mysql_conn.connect();
- 
-        mysql_conn.query('SELECT * from menu', function (error, results, fields) {
-        if (error) throw error;
-            console.log('The solution is: ', results[0].solution);
-        });
-        reply(results);
-        mysql_conn.end();
-    }
-});
-
-
-
-server.start((err) => {
-
+//Load plugins and start server
+server.register([
+    require('./routes/dishes')
+], (err) => {
     if (err) {
         throw err;
     }
-    console.log(`Server running at: ${server.info.uri}`);
+    // Start the server
+    server.start((err) => {
+        console.log('Server running at:', server.info.uri);
+    });
+
 });
+
+// server.route({
+//     method: 'GET',
+//     path: '/wuliang_order',
+//     handler: function (request, reply) {
+//         reply('wu liang order home!');
+//     }
+// });
+
+// server.route({
+//     method: 'GET',
+//     path: '/wuliang_order/menu/list_all',
+//     handler: function (request, reply) {
+//         reply(results);
+//     }
+// });
+
+// server.route({
+//     method: 'GET',
+//     path: '/wuliang_order/{module}',
+//     handler: function (request, reply) {
+//         // reply('Module -- ' + encodeURIComponent(request.params.module) + ' under developing!');
+//         mysql_conn.connect();
+ 
+//         mysql_conn.query('SELECT * from menu', function (error, results, fields) {
+//         if (error) throw error;
+//             console.log('The solution is: ', results[0].solution);
+//         });
+//         reply(results);
+//         mysql_conn.end();
+//     }
+// });
+
+
+
+// server.start((err) => {
+
+//     if (err) {
+//         throw err;
+//     }
+//     console.log(`Server running at: ${server.info.uri}`);
+// });
